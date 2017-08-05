7297af73f5075d4d7b02def54fa1d8b082701288
Howard277 <wuketao277@sohu.com>
2017-08-05 21:31:43 +0800

    添加controller

--- main/main.go
+++ main/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main()  {
-	print("test")
+import "github.com/astaxie/beego"
+
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get() {
+	this.Ctx.WriteString("hello world")
+}
+
+func main() {
+	beego.Router("/", &MainController{})
+	beego.Run()
 }
